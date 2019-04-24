c91b2d8b56c587dd7177d3168659b2f53504d01e
Nishio Yoshiki <nishio.yoshiki@gmail.com>
2019-04-24 15:49:56 +0900

    覚えたゾ

--- mypage/config/routes.rb
+++ mypage/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'movie/index'
+  get 'list/new'
+  get 'list/show'
+  get 'friends/new'
+  get 'friends/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #match 'hello', to:  'list#hello' , via:[:get, :post]
+  #match 'bye' ,to: 'list#bye' , via:[:get, :post]
 end
